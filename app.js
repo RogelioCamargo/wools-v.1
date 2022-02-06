ee7ddbcff8da28d62755854f6bfe18c84e321724
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-02-05 17:50:29 -0800

    Containerize entire app

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require("cors");
-require("dotenv").config();
 const passport = require("passport");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
